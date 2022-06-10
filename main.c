2e84e68d18c12ba048b03303d49f9bd9cd7cb404
MessiasPerugiaBraga <102391036+MessiasPerugiaBraga@users.noreply.github.com>
2022-06-10 20:35:42 -0300

    main.c
    Primeiro commite

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MinhaBiblioteca.h"
+
+int main(void)
+{
+
+    /*Retangulo r1 = {1.5, 3.0}, r2 = {4.0, 5.0};
+    Circulo c1, c2;
+    double p, a;
+    
+    c1.raio = 5.0;
+    c2.raio = 1.0/MEU_PI;
+
+    printf("%.4lf\n", PHI);
+    printf("%c\n", minha_global);
+
+    p = perimetro_retangulo(r1);
+    a = area_retangulo(r1);
+    printf("R1 - P: %.2lf A: %.2lf\n", p, a);
+
+    p = perimetro_retangulo(r2);
+    a = area_retangulo(r2);
+    printf("R1 - P: %.2lf A: %.2lf\n", p, a);
+
+    p = perimetro_circulo(c1);
+    a = area_circulo(c1);
+    printf("C1 - P: %.2lf A: %.2lf\n", p, a);
+
+    p = perimetro_circulo(c2);
+    a = area_circulo(c2);
+    printf("C2 - P: %.2lf A: %.2lf\n", p, a);
+
+    return 0;
+    */
+}

8ed78e2b4191a309f109fd99beb124795fa7077f
MessiasPerugiaBraga <102391036+MessiasPerugiaBraga@users.noreply.github.com>
2022-06-10 20:37:44 -0300

    MinhaBiblioteca.h

---
+++ MinhaBiblioteca.h
@@ -0,0 +1,22 @@
+#ifndef _TROCA_VETOR_H
+#define _TROCA_VETOR_H
+
+/*#define PHI 1.61803399
+extern const double MEU_PI;
+extern char minha_global;
+
+typedef struct {
+    double largura, comprimento;
+}Retangulo;
+
+typedef struct {
+    double raio;
+}Circulo;
+
+double perimetro_retangulo(Retangulo);
+double area_retangulo(Retangulo);
+double perimetro_circulo(Circulo);
+double area_circulo(Circulo);
+*/
+
+#endif

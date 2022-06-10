2c1548c0cef6ac27b0f1493d5dff17da1fb16875
MessiasPerugiaBraga <102391036+MessiasPerugiaBraga@users.noreply.github.com>
2022-06-10 20:37:02 -0300

    MinhaBiblioteca.c

---
+++ MinhaBiblioteca.c
@@ -0,0 +1,31 @@
+#include "MinhaBiblioteca.h"
+
+int numTrocas(int a)
+{
+    int *vetCaract;
+    
+}
+
+/*const double MEU_PI = 3.14159265;
+char minha_global = 'i';
+
+double perimetro_retangulo(Retangulo r){
+
+    return 2 * (r.comprimento + r.largura);
+}
+
+double area_retangulo(Retangulo r){
+
+    return r.comprimento * r.largura;
+}
+
+double perimetro_circulo(Circulo c){
+
+    return 2 * MEU_PI * c.raio;
+}
+
+double area_circulo(Circulo c){
+
+    return MEU_PI * c.raio * c.raio;
+}
+*/
